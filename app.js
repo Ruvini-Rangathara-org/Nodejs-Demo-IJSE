b1e069c3d0a1eba82e041c7b48c6247d5f7b1b01
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-12-23 13:37:50 +0530

    sync vs async

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const fs = require ('fs');
+const fs = require('fs');
 
 //after ECMAScript 6
 //before ECMAScript 6 use import
 
 
-fs.readFile('doc.txt', 'utf-8', (err, data) => {
-    if(err) throw err;
-    console.log(data);
-});
+//asynchronous method
+//firstly print the end of the async program and then print the content of the file
+// fs.readFile('doc.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+// console.log("End of the async program");
 
 
 //read as Buffer
-fs.readFile('doc.txt', (err, data) => {
-    if(err) throw err;
-    console.log(data);
-});
+// fs.readFile('doc.txt', (err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+
+//synchronous method
+const read = fs.readFileSync('doc.txt', 'utf-8');
+//read as Buffer
+const read2 = fs.readFileSync('doc.txt');
+console.log(read);
+console.log(read2);
+console.log("End of the sync program");
