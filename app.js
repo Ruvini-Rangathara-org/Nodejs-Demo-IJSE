f8f9edfbe04d6380a909d16599ba86eb2c7f9d20
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-12-23 11:39:20 +0530

    nodemon

--- app.js
+++ app.js
@@ -3,7 +3,49 @@ console.log("Welcome to Node.js");
 const name = "IJSE";
 console.log("Institute : "+name);
 
+//open cmd in this directory
+//type to run node app.js
+
 // Global Object
-console.log(global);
-global.setTimeout(() => {console.log("Timeout!!")}, 4000);
-global.setInterval(() => {console.log("Interval!!")}, 4000);
+// console.log(global);
+
+// global.setTimeout(() => {console.log("Timeout!!")}, 4000);
+// global.setInterval(() => {console.log("Interval!!")}, 4000);
+
+//enviroment variables
+console.log("Port : "+process.env.PORT);
+console.log("Username : "+process.env.USERNAME);
+console.log("Path : "+process.env.PATH);
+console.log("Directory name : "+__dirname);
+console.log("File name : "+__filename);
+
+
+//in terminal
+//npm init
+//nodemon
+//npm install --save-dev nodemon
+
+//package.json
+// {
+//     "name": "nodejs-demo",
+//     "version": "1.0.0",
+//     "description": "",
+//     "main": "app.js",
+//     "scripts": {
+//     "test": "echo \"Error: no test specified\" && exit 1",
+//===============================================================
+//         "start": "nodemon app.js"
+// },
+//     "author": "",
+//     "license": "ISC",
+//     "devDependencies": {
+//     "nodemon": "^3.0.2"
+// }
+// }
+
+//in cmd , npm start
+
+
+console.log("Welcome to Nodemon");
+console.log("Welcome again");
+console.log("Welcome to Node.js");
